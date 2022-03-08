49d5c9b5020b73be167f10c69de627d13240eb7c
paulleerentveld <paul@veldnet.com.au>
2022-03-08 14:55:47 +1000

    add functions for lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+
+
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0,2);
+}
+
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(-2)
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const createFareMultiplier = (a) => {
+    return (a) =>a **2;
+}
+
+const fareDoubler = (a) => {
+    return a * 2;
+
+}
+
+const fareTripler = (a) => {
+    return a * 3;
+
+}
+
+const selectDifferentDrivers = (drivers,fn) => {
+    return fn(drivers);
+
+}
